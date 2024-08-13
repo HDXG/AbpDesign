35c4091ef9d58f0bb5be1e1246c5ee4b3b27f682
han <hh1329207@163.com>
2024-08-13 22:15:06 +0800

    111

---
+++ FrameWork/Design.Application/DesignApplicationModule.cs
@@ -0,0 +1,15 @@
+﻿using Design.Application.Contracts;
+using Volo.Abp.Application;
+using Volo.Abp.Modularity;
+
+namespace Design.Application
+{
+    [DependsOn(
+        typeof(AbpDddApplicationModule),
+        typeof(DesignApplicationContractsModule)
+    )]
+    public class DesignApplicationModule:AbpModule
+    {
+
+    }
+}
---
+++ FrameWork/Design.Application/Services/DesignApplicationService.cs
@@ -0,0 +1,9 @@
+﻿using Volo.Abp.Application.Services;
+
+namespace Design.Application.Services
+{
+    public class DesignApplicationService:ApplicationService
+    {
+
+    }
+}
